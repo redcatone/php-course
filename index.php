38262965d4a49d4d0ae624fd9513e4e4985ffd3d
calvin <redcatone1@gmail.com>
2020-02-24 06:51:13 +0800

    Add tax_calc function

--- index.php
+++ index.php
@@ -8,15 +8,15 @@
       // Using two forward slashes we create comments in php.
       // Within the php tag, we can insert values into our HTML.
       $name = "PHP Store";
-      $credit = 1000; 
+      $credit = 1000;
 
       echo "<h1>Welcome to ".$name."!</h1>";
       echo "<h2>You have $".$credit." in your wallet.</h2>";
 
-	    $products['Computer']=750;
-	    $products['Car']=15000;
-	    $products['iPhone']=1000;
-	    $products['Toaster']=75;
+	    $products['Computer'] = 750;
+	    $products['Car'] = 15000;
+	    $products['iPhone'] = 1000;
+	    $products['Toaster'] = 75;
       foreach($products as $key => $value){
         echo "<p>The ".$key." costs $".$value."</p>";
       }
@@ -27,6 +27,18 @@
             echo "<p>".$key."</p>";
           }
         }
+
+      $amount = 800;
+      $taxRate = 0.0825;
+      $addedTax = $amount * $taxRate;
+      // echo $addedTax;
+      echo tax_calc(750, 0.223);
+      function tax_calc($amount, $tax){
+        $calculate_tax = $amount * $tax;
+        $amount = round($amount + $calculate_tax, 2);
+        return $amount;
+      }
+
     ?>
   </body>
 </html>
